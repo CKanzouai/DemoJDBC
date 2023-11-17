6c31087c2b99dbafbe2c039fc8c6330bfa1760b0
chaimae <chaimae.kanzouai@usmba.ac.ma>
2023-11-17 16:40:26 +0100

    insert select commit

--- src/main/java/fr/digi/m0923/Main.java
+++ src/main/java/fr/digi/m0923/Main.java
@@ -1,43 +1,104 @@
 package fr.digi.m0923;
+
 import java.sql.*;
 import java.util.ResourceBundle;
 
 public class Main {
 
-       // System.out.println("Hello world!");
-        private static final String  DB_URL;
-        private static final String DB_USER;
-        private static final String DB_PWD;
+    private static final String DB_URL;
+    private static final String DB_USER;
+    private static final String DB_PWD;
 
-        static {
-            System.out.println("block static");
-            ResourceBundle bundle = ResourceBundle.getBundle("db");
-            DB_URL = bundle.getString("db.url");
-            DB_USER = bundle.getString("db.user");
-            DB_PWD = bundle.getString("db.password");
-        }
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("db");
+        DB_URL = bundle.getString("db.url");
+        DB_USER = bundle.getString("db.user");
+        DB_PWD = bundle.getString("db.password");
+    }
+
     public static void main(String[] args) {
-
         Connection connection = null;
 
         try {
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD);
-            System.out.println("connexion a la BD réussite!");
+            System.out.println("Connexion à la base de données réussie!");
             System.out.println(connection);
 
+            // creation();
+
+            // Exemple de lecture de données
+            lecture();
+
+
+
         } catch (SQLException e) {
-            throw new RuntimeException("Attention" +e.getMessage());
+            throw new RuntimeException("Attention : " + e.getMessage());
         } finally {
-            if (null != connection){
+            if (connection != null) {
                 try {
                     connection.close();
-                }catch (SQLException e){
+                } catch (SQLException e) {
                     System.out.println("Attention : " + e.getMessage());
                 }
             }
         }
+    }
 
+    private static void creation() {
+        Connection connection = null;
 
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD);
+            System.out.println("Connexion à la base de données réussie!");
+            System.out.println(connection);
 
+            Statement st = connection.createStatement();
+            int nb = st.executeUpdate("INSERT INTO FOURNISSEUR (NOM) VALUES ('DIGI')");
+            System.out.println("Nombre d'éléments modifiés : " + nb);
+
+            st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("Attention : " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    System.out.println("Attention : " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    private static void lecture() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD);
+            System.out.println("Connexion à la base de données réussie!");
+            System.out.println(connection);
+
+            Statement st = connection.createStatement();
+            ResultSet curseur = st.executeQuery("SELECT ID, NOM FROM FOURNISSEUR");
+
+            while (curseur.next()) {
+                Integer id = curseur.getInt("ID");
+                String nom = curseur.getString("NOM");
+                System.out.println(id + " " + nom);
+            }
+
+            curseur.close();
+            st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("Attention : " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    System.out.println("Attention : " + e.getMessage());
+                }
+            }
+        }
     }
 }
